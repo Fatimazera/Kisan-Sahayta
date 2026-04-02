be3929b4bf0efd28b60e608141882d56af4119d8
fatima uz zehra <fzehra1801@gmail.com>
2026-04-02 06:37:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-123246316-beb4e",
+  "appId": "1:320666024373:web:2492c52b802db71e4f81fc",
+  "apiKey": "AIzaSyCPpindpi3EzlcFIUmiPQImS4KTxGywh18",
+  "authDomain": "studio-123246316-beb4e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "320666024373"
+};
